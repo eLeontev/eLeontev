270400aa72b697fd62287d763e4319fba1cc4e09
eLeontev <joonior199@gmail.com>
2020-03-20 01:20:55 +0300

    Add base enem calculation

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,11 +1,14 @@
 import { Game, MiddleCoordinate, direction } from '../model/game.model';
 import { getRadians } from '../helpers/radiant-transformer';
+import { randomIntegerInRange } from '../helpers/randomizer';
 
 const { clockwise, сСlockwise } = direction;
 const [{ width, height }]: DOMRectList = document.body.getClientRects();
 const canvasSize = width > height ? height : width;
 const canvasMiddlePosition = canvasSize / 2;
 const radius = canvasMiddlePosition * 0.9;
+const innerRadius = radius / 3;
+const minimumEnemyOffset = 20;
 
 const canvasMiddlePoint: MiddleCoordinate = {
     x: canvasMiddlePosition,
@@ -30,7 +33,7 @@ const setStaticFigures = (canvasCtx: CanvasRenderingContext2D) => {
     canvasCtx.stroke();
 
     canvasCtx.beginPath();
-    canvasCtx.arc(x, y, radius / 3, 0, getRadians(360));
+    canvasCtx.arc(x, y, innerRadius, 0, getRadians(360));
     canvasCtx.stroke();
 };
 
@@ -59,13 +62,60 @@ const drowPointer = (angle: number, canvasCtx: CanvasRenderingContext2D, usedCol
     canvasCtx.stroke();
 };
 
-const setPosition = () =>
+const cleanUpPreviosPointer = (canvasCtx: CanvasRenderingContext2D) => {
+    canvasCtx.lineWidth = 4 + 2;
+    canvasCtx.strokeStyle = 'white';
+    canvasCtx.stroke();
+};
+
+const performPointerItaration = (canvasCtx: CanvasRenderingContext2D) => {
+    cleanUpPreviosPointer(canvasCtx);
+    setStaticFigures(canvasCtx);
+    drowPointer(angle - pointerDirection, canvasCtx, 'white', true);
+    drowPointer(angle, canvasCtx, 'blue', false);
+    angle = angle + pointerDirection;
+};
+
+const calclulateEnemy = (angle: number) => {
+    const minEnemyPosition = Math.abs(angle % 360) + minimumEnemyOffset;
+    const maxEnemyPosition = minEnemyPosition + 360 - minimumEnemyOffset;
+
+    const middlePointAngle = randomIntegerInRange(minimumEnemyOffset, maxEnemyPosition);
+    const distanceFromMiddlePoint = randomIntegerInRange(innerRadius, radius * 0.9);
+    const enemyRadius = randomIntegerInRange(innerRadius * 0.1, innerRadius * 0.4);
+
+    const angleRad = getRadians(middlePointAngle);
+    const xPosition = distanceFromMiddlePoint * Math.sin(angleRad) + x;
+    const yPosition = distanceFromMiddlePoint * Math.cos(angleRad) + y;
+
+    return {
+        xPosition,
+        yPosition,
+        enemyRadius
+    };
+};
+
+let enemyCoords;
+const drowEnemy = (angle: number, canvasCtx: CanvasRenderingContext2D) => {
+    if (!enemyCoords) {
+        enemyCoords = calclulateEnemy(angle);
+    }
+
+    const { xPosition, yPosition, enemyRadius } = enemyCoords;
+
+    canvasCtx.beginPath();
+    canvasCtx.strokeStyle = 'red';
+
+    canvasCtx.arc(xPosition, yPosition, enemyRadius, 0, getRadians(360));
+    canvasCtx.stroke();
+};
+
+const setPosition = () => {
     setInterval(() => {
-        setStaticFigures(ctx);
-        drowPointer(angle - pointerDirection, ctx, 'white', true);
-        drowPointer(angle, ctx, 'blue', false);
-        angle = angle + pointerDirection;
+        performPointerItaration(ctx);
+        drowEnemy(angle, ctx);
     }, 10);
+};
 
 setButtonListener(changePointerDirection);
 setPosition();
---
+++ src/helpers/randomizer.ts
@@ -0,0 +1,1 @@
+export const randomIntegerInRange = (min: number, max: number): number => Math.floor(min + Math.random() * (max + 1 - min));
